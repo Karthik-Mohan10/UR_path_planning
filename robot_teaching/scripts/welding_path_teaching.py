08a3cc6916c9914790ae341cd826296b45d15c56
Karthik-Mohan10 <karthikvv.1010@gmail.com>
2024-06-08 15:53:20 +0200

    Format update

--- robot_teaching/scripts/welding_path_teaching.py
+++ robot_teaching/scripts/welding_path_teaching.py
@@ -41,38 +41,13 @@ def ar_tag_estimate_callback(msg):
     except:
         print("AR not found")
         
-'''
-def transform_marker(msg):
-    global tfbuffer
-    
-    target_frame = "base_link"
-    
-    try:
-        trans = tfbuffer.can_transform(target_frame, msg.header.frame_id, rospy.Time(0))
-    except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException) as e: 
-        rospy.logwarn(f"Transsform lookup failed: {e}")
-        #return 
-    #pose = msg.markers[0].pose
-    #pose.header.frame_id = "usb_cam"
-    msg.header.stamp = rospy.Time(0)
-    
-    try:
-        pose_transformed = tfbuffer.transform(msg, target_frame, rospy.Duration(1))
-        rospy.loginfo(f"Transformed pose in {target_frame} : {pose_transformed}")
-    except Exception as e:
-        print(e)
-    return pose_transformed
-'''
+
 def main():
 
     global pose_list, pose, tfbuffer, listener
     tfbuffer = tf2_ros.Buffer(rospy.Duration(2.0))
     listener = tf2_ros.TransformListener(tfbuffer)
     tfbroadcast = tf2_ros.TransformBroadcaster()
-
-    #nozzle_transform = tfbuffer.lookup_transform("tool0", "nozzle_link", rospy.Time(0), rospy.Duration(0.5))
-    #nozzle_transform.header.frame_id = "ar_marker_13a"
-    #nozzle_transform.child_frame_id = "tcp_goal"
   
 
     while True:
@@ -91,30 +66,13 @@ def main():
             pose.header = t.header
             pose.pose.orientation = t.transform.rotation
             pose.pose.position = t.transform.translation  
+            #Offset in z direction to maintain necessary gap with the ground
             pose.pose.position.z += .03
             #pose.pose.orientation.x = 0
             #pose.pose.orientation.y = 0
 
-        
-
             print("Transformed pose-------")
             print(pose)
-
-            #pose.pose.position.z = pose.pose.position.z -0.098  #nozzle z offset
-
-
-
-            #print("Updated pose--------------")
-            #print(pose)
-
-            # offset_pose = tf2_geometry_msgs.do_transform_pose(pose, nozzle_transform)
-            # print("----")
-            # print(offset_pose)
-
-            #nozzle_t = TransformStamped()
-            #nozzle_t.transform.translation.z = 0.1
-            
-            #pose_updated = transform_marker(p)
             
             pose_list.append(pose)
     
